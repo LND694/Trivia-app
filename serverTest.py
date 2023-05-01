7961fd2fe2ccb1b6a59f844ab2799c8666280288
LEVIS <yoavlevy1000@gmail.com>
2023-05-01 07:52:03 +0300

    resolved threads for the CR

--- serverTest.py
+++ serverTest.py
@@ -1,18 +1,26 @@
 import socket
-host = socket.gethostname()  # as both code is running on same pc
-port = 8265  # socket server port number
-
-if port < 1024 or port > 65535:
-    exit()
-
-client_socket = socket.socket()  # instantiate
-client_socket.connect((host, port))  # connect to the server
-data = client_socket.recv(1024).decode()
-print('Received from server: ' + data)  # show in terminal
-message = "hey boaz"
-client_socket.send(message.encode())
 
 
-client_socket.close()  # close the connection
+def main():
+
+    host = socket.gethostname()  # as both code is running on same pc
+    MIN_PORT = 1024
+    MAX_PORT = 65535
+    port = 8265  # socket server port number
+
+    if port < MIN_PORT or port > MAX_PORT:
+        exit()
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+    data = client_socket.recv(1024).decode()
+    print('Received from server: ' + data)  # show in terminal
+    message = "hey boaz"
+    client_socket.send(message.encode())
 
 
+    client_socket.close()  # close the connection
+
+
+if __name__ == "__main__":
+    main()
