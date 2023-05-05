a25dd73e5721690c983eef6ccfe9460e9a60f333
boaz sarel <boaz.sarel@gmail.com>
2023-05-05 18:33:47 +0300

    IRequestHandler with 2 more functions

--- Server/IRequestHandler.h
+++ Server/IRequestHandler.h
@@ -1,10 +1,31 @@
 #pragma once
 
+#include <ctime>
+#include "global.h"
+#include "JsonResponsePacketSerializer.h"
+
+using std::time_t;
+
+class IRequestHandler; //pre definition
+
+typedef struct RequestInfo
+{
+	RequestId id;
+	time_t receivalTime;
+	Buffer buffer;
+} RequestInfo;
+
+typedef struct RequestResult
+{
+	Buffer response;
+	IRequestHandler* newHandler;
+} RequestResult;
+
 
 class IRequestHandler
 {
 public:
-
-private:
-
+	//Functions
+	virtual bool isRequestRelevent(const RequestInfo& requestInfo) = 0;	
+	virtual RequestResult& handleRequest(const RequestInfo& requestInfo) = 0;
 };
