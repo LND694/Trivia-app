49ee87c4a8fbcab2d09adb261e9d45a32c1183f1
boaz sarel <boaz.sarel@gmail.com>
2023-05-10 18:30:48 +0300

    Sqlite functions

---
+++ Server/IDatabase.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "sqlite3.h"
+
+
+using std::string;
+
+class IDatabase
+{
+public:
+	//Open&Close functions 
+	virtual bool open() = 0;
+	virtual bool close() = 0;
+
+	//User functions
+	virtual int doesUserExist(const string username) = 0;
+	virtual int doesPasswordMatch(const string username, const string password) = 0;
+	virtual int addNewUser(const string username, const string password, const string email) = 0;
+
+};
