4f81dcf93b15a4439b001b686efdef3827a06646
boaz sarel <boaz.sarel@gmail.com>
2023-05-19 15:09:35 +0300

    New Consts and Response structs

--- Server/Response.h
+++ Server/Response.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Room.h"
+
+using std::string;
+using std::vector;
 
 constexpr int OK_STATUS_CODE = 1;
-const std::string ERROR_MSG = "ERROR";
+const string ERROR_MSG = "ERROR";
 
 //Base class
 class Response
@@ -11,10 +17,20 @@ class Response
 };
 
 //Base struct
-struct ResponseWithStatus : Response
+typedef struct ResponseWithStatus : Response
 {
 	unsigned int status;
-};
+} ResponseWithStatus;
+
+typedef struct ResponseWithStatistics : Response
+{
+	vector<string> statistics;
+} ResponseWithStatistics;
+
+typedef struct ResponseWithPlayers : Response
+{
+	vector<string> players;
+} ResponseWithPlayers;
 
 //Response structs
 typedef struct LoginResponse : ResponseWithStatus
@@ -27,7 +43,44 @@ typedef struct SignUpResopnse : ResponseWithStatus
 
 } SignUpResponse;
 
+typedef struct LogoutResponse : ResponseWithStatus
+{
+
+} LogoutResponse;
+
+typedef struct GetRoomsResponse : ResponseWithStatus
+{
+	vector<RoomData> rooms;
+} GetRoomsResponse;
+
+typedef struct GetPlayersInRoomResponse : ResponseWithPlayers
+{
+
+} GetPlayersInRoomResponse;
+
+typedef struct GetHighScoreResponse : ResponseWithStatus, ResponseWithStatistics
+{
+
+} GetHighScoreResponse;
+
+typedef struct GetPersonalStatsResponse : ResponseWithStatus, ResponseWithStatistics
+{
+
+} GetPersonalStatsResponse;
+
+typedef struct JoinRoomResponse : ResponseWithStatus 
+{
+
+} JoinRoomResponse;
+
+typedef struct CreateRoomResponse : ResponseWithStatus
+{
+
+} CreateRoomResponse;
+
+
+
 typedef struct ErrorResopnse : Response
 {
-	std::string message;
+	string message;
 } ErrorResponse;
