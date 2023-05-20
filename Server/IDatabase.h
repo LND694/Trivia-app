5333370e6615c5ca51769a5919c282526c1cfcc5
boaz sarel <boaz.sarel@gmail.com>
2023-05-20 22:03:26 +0300

    New interfase for IDatabase classes

--- Server/IDatabase.h
+++ Server/IDatabase.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <iostream>
+#include <list>
 #include "User.h"
+#include "Question.h"
 #include "sqlite3.h"
 #include "global.h"
 #include "Singleton.h"
 
 using std::string;
+using std::list;
 enum RETURNED_CODES {
 	ERROR_CODE = -1, OK_CODE = 0,
 	USER_EXIST = 1, USER_NOT_EXIST = 2,
@@ -24,4 +27,13 @@ public:
 	virtual int doesUserExist(const string username) = 0;
 	virtual int doesPasswordMatch(const string username, const string password) = 0;
 	virtual int addNewUser(const User& user) = 0;
+
+	//Question functions
+	virtual list<Question>& getQuestions(const int amountQuestions) = 0;
+	virtual float getPlayerAverageAnswerTime(const string player) = 0;
+	virtual int getNumOfCorrectAnswers(const string player) = 0;
+	virtual int getNumOfTotalAnswers(const string player) = 0;
+	virtual int getNumOfPlayerGames(const string player) = 0;
+	virtual int getPlayerScore(const string player) = 0;
+	virtual vector<string> getHighScores() = 0;
 };
