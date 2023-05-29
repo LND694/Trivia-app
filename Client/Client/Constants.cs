c1b976f030f7d6d1a7226f08cc4e9cb73ab31709
boaz sarel <boaz.sarel@gmail.com>
2023-05-30 00:03:44 +0300

    Constants class for the program

---
+++ Client/Client/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Constants
+    {
+        public const int OK_STATUS_CODE = 1;
+        public const int SIZE_CODE_FIELD = 3;
+        public const int SIZE_LENGTH_DATA_FIELD = 10;
+        public const char SPACE = ' ';
+        public const char NEW_LINE = '\n';
+        public const char END_STR_SYMBOL = '\0';
+    }
+}
