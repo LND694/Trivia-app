ec1b01a7eb0f778ac47ea8522ae8fa4832647dc6
boaz sarel <boaz.sarel@gmail.com>
2023-05-31 15:30:22 +0300

    Infinty Loop Server

--- Server/Communicator.cpp
+++ Server/Communicator.cpp
@@ -94,30 +94,32 @@ void Communicator::handleNewClient(SOCKET socket)
 	string code;
 	Buffer* data;
 	this->m_clients.insert({ socket, this->m_handlerFactory->createLoginRequestHandler()});//init a new pair of the given socket and a login request since it is a new user
-	len = recv(socket, buffer, MAX_SIZE - 1, NULL);//MAX_SIZE-1 forthe null terminator
-
-	Buffer charVector(buffer, buffer + MAX_SIZE);
-	charVector[len] = '\0';//add null terminator
-
-	data = getDataFromBuffer(charVector);
-	//Extracting the code from the request's buffer
-	for (int i = 0; i < SIZE_CODE_FIELD; i++)
+	while (this->m_clients.at(socket) != nullptr)
 	{
-		code += data->at(i);
+		len = recv(socket, buffer, MAX_SIZE - 1, NULL);//MAX_SIZE-1 forthe null terminator
+
+		Buffer charVector(buffer, buffer + MAX_SIZE);
+		charVector[len] = '\0';//add null terminator
+
+		data = getDataFromBuffer(charVector);
+		//Extracting the code from the request's buffer
+		for (int i = 0; i < SIZE_CODE_FIELD; i++)
+		{
+			code += data->at(i);
+		}
+
+		//turn the buffer into request
+		info.buffer = *data;
+		info.receivalTime = time(nullptr);//get the current time
+		info.id = static_cast<RequestId>(atoi(code.c_str()));
+
+		//get the response
+		res = this->m_clients.at(socket)->handleRequest(info);
+		cout << res.response.data() << endl;
+
+		//send the response
+		send(socket, reinterpret_cast<char*>(res.response.data()), static_cast<int>(res.response.size()), NULL);
 	}
-
-	//turn the buffer into request
-	info.buffer = *data;
-	info.receivalTime = time(nullptr);//get the current time
-	info.id = static_cast<RequestId>(atoi(code.c_str()));
-
-	//get the response
-	res = this->m_clients.at(socket)->handleRequest(info);
-	cout << res.response.data() << endl;
-
-	//send the response
-	send(socket, reinterpret_cast<char*>(res.response.data()), static_cast<int>(res.response.size()),NULL);
-
 }
 
 /// <summary>
