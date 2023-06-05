13a6c7614c4515394cee347dd3dad2d92b20a6f1
Yoav Levy <yoavlevy1000@gmail.com>
2023-06-05 20:54:35 +0300

    added room member functions

--- Server/RoomMemberRequestHandler.cpp
+++ Server/RoomMemberRequestHandler.cpp
@@ -2,11 +2,38 @@
 
 bool RoomMemberRequestHandler::isRequestRelevent(const RequestInfo& requestInfo)
 {
-    return false;
+    return requestInfo.id == LEAVE_ROOM_REQS_CODE ||requestInfo.id == GET_ROOMS_REQS_CODE;
 }
 
 RequestResult& RoomMemberRequestHandler::handleRequest(const RequestInfo& requestInfo)
 {
-    RequestResult* req = new RequestResult();
-    return *req;
+    if (requestInfo.id == LEAVE_ROOM_REQS_CODE)
+    {
+        return leaveRoom(requestInfo);
+    }
+}
+
+RequestResult& RoomMemberRequestHandler::leaveRoom(const RequestInfo& requestInfo)
+{
+    LeaveRoomResponse leaveResp = LeaveRoomResponse();
+    RequestResult* reqRes = new RequestResult();
+    this->m_room.removeUser(this->m_user);
+    leaveResp.status = OK_CODE;
+    reqRes->response = JsonResponsePacketSerializer::serializeResponse(leaveResp);
+    reqRes->newHandler = this->m_handlerFactory.createMenuRequestHandler(this->m_user);
+    return *reqRes;
+}
+
+RequestResult& RoomMemberRequestHandler::getRoomState(const RequestInfo& requestInfo)
+{
+    RequestResult* reqRes = new RequestResult();
+    GetRoomStateResponse stateResp = GetRoomStateResponse();
+    stateResp.hasGameBegun = this->m_roomManager.getRoomState(this->m_room.getRoomData().id);
+    stateResp.AnswerCount = this->m_room.getRoomData().numOfQuestionsInGame;
+    stateResp.answerTimeOut = this->m_room.getRoomData().timePerQuestion;
+    stateResp.players = this->m_room.getAllUsers();
+    stateResp.status = OK_CODE;
+    reqRes->response = JsonResponsePacketSerializer::serializeResponse(stateResp);
+    reqRes->newHandler = this;
+    return *reqRes;
 }
--- Server/RoomMemberRequestHandler.h
+++ Server/RoomMemberRequestHandler.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "IRequestHandler.h"
-
+#include "RoomManager.h"
+#include "RequestHandlerFactory.h"
 class RoomMemberRequestHandler : public IRequestHandler
 {
+protected:
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+	RequestResult& getRoomState(const RequestInfo& requestInfo);
 public:
 	//Functions
 	bool isRequestRelevent(const RequestInfo& requestInfo) override;
 	RequestResult& handleRequest(const RequestInfo& requestInfo) override;
+private:
+	RequestResult& leaveRoom(const RequestInfo& requestInfo);
 };
 
