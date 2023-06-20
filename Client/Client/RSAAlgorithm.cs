7b7fb294719c8d02bde8a7679bbd9266a06b2bea
boaz sarel <boaz.sarel@gmail.com>
2023-06-21 00:45:42 +0300

    Changing from XML public key to a PEM one

--- Client/Client/RSAAlgorithm.cs
+++ Client/Client/RSAAlgorithm.cs
@@ -5,21 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
 
 namespace Client
 {
     public class RSAAlgorithm : CryptoAlgoritm
     {
-        private RSACryptoServiceProvider rsa;
         private RSAParameters privateKey;
         private RSAParameters publicKey;
-        private const string DIRECTORY_PATH_SERVER_KEY = "C:/Keys/ServerKey.txt";
+        private const string DIRECTORY_PATH_SERVER_KEY = "C:/Keys/ServerKey.pem";
         private RSAParameters serverPublicKey;
 
         public RSAAlgorithm():
             base()
         {
-            this.rsa = new RSACryptoServiceProvider();
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             this.privateKey = rsa.ExportParameters(true);
             this.publicKey = rsa.ExportParameters(false);
 
@@ -45,46 +48,64 @@ namespace Client
 
         public override byte[] Encrypt(string message, string key)
         {
-            // Convert the message to bytes
-            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(this.serverPublicKey);
 
-            // Encrypt the message using RSA encryption
-            byte[] encryptedBytes = this.rsa.Encrypt(messageBytes, true);
+                // Convert the message to bytes
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
-            return encryptedBytes;
+                // Encrypt the message using RSA
+                byte[] encryptedBytes = rsa.Encrypt(messageBytes, true);
+
+                return encryptedBytes;
+            }
         }
 
         public override string GenerateKey()
         {
-            //Getting the string of the public key
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            rsa.ImportParameters(this.publicKey);
-            return rsa.ToXmlString(false);
+            ////Getting the string of the public key
+            //RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            //rsa.ImportParameters(this.publicKey);
+            //return rsa.ToXmlString(false);
+
+            StringBuilder sb = new StringBuilder();
+
+            // Add the PEM header
+            sb.AppendLine("-----BEGIN PUBLIC KEY-----");
+
+            // Encode the modulus and exponent in base64
+            string modulusBase64 = Convert.ToBase64String(publicKey.Modulus);
+            string exponentBase64 = Convert.ToBase64String(publicKey.Exponent);
+
+            // Add the modulus and exponent lines
+            sb.AppendLine(modulusBase64);
+            sb.AppendLine(exponentBase64);
+
+            // Add the PEM footer
+            sb.AppendLine("-----END PUBLIC KEY-----");
+
+            return sb.ToString();
         }
 
         private static RSAParameters ReadPublicKeyFromFile(string filePath)
         {
-            try
+            string pemContents = File.ReadAllText(filePath);
+
+            using (var stringReader = new StringReader(pemContents))
             {
-                // Read the contents of the public key file
-                string publicKeyXml = File.ReadAllText(filePath);
+                var pemReader = new PemReader(stringReader);
+                var publicKeyObject = pemReader.ReadObject();
 
-                // Create an RSA object
-                RSA rsa = RSA.Create();
-
-                // Import the public key from the XML string
-                rsa.FromXmlString(publicKeyXml);
-
-                // Extract the RSA parameters
-                RSAParameters publicKeyParams = rsa.ExportParameters(false);
-
-                return publicKeyParams;
-            }
-            catch (Exception ex)
-            {
-                // Handle any exceptions
-                Console.WriteLine("Error reading public key from file: " + ex.Message);
-                return default(RSAParameters);
+                if (publicKeyObject is RsaKeyParameters rsaKeyParameters)
+                {
+                    var rsaParameters = DotNetUtilities.ToRSAParameters(rsaKeyParameters);
+                    return rsaParameters;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid RSA public key format.");
+                }
             }
         }
     }
