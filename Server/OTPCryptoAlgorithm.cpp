ef1bd1f771fe1648dd0796ffe74547e8e49571f3
boaz sarel <boaz.sarel@gmail.com>
2023-06-28 15:19:21 +0300

    Trying to do xor by hand

--- Server/OTPCryptoAlgorithm.cpp
+++ Server/OTPCryptoAlgorithm.cpp
@@ -2,28 +2,26 @@
 
 string OTPCryptoAlgorithm::encrypt(const string message, const byte* key) const
 {
-    CryptoPP::SecByteBlock keyBytes(reinterpret_cast<const CryptoPP::byte*>(key), OTP_KEY_SIZE);
-
-    CryptoPP::SecByteBlock plaintextBytes(reinterpret_cast<const CryptoPP::byte*>(message.data()), message.size());
-
-    CryptoPP::SecByteBlock encryptedBytes(plaintextBytes.size());
-    CryptoPP::xorbuf(encryptedBytes, plaintextBytes, keyBytes, plaintextBytes.size());
-
-    string encryptedText(encryptedBytes.begin(),encryptedBytes.end());
-
-    return encryptedText;
+    return this->decrypt(message, key);
 }
 
 string OTPCryptoAlgorithm::decrypt(const string message, const byte* key) const
 {
     string realText;
-    CryptoPP::SecByteBlock keyBytes(reinterpret_cast<const CryptoPP::byte*>(key), OTP_KEY_SIZE);
-    CryptoPP::SecByteBlock encryptedBytes(reinterpret_cast<const CryptoPP::byte*>(message.data()), message.size());
+    //CryptoPP::SecByteBlock keyBytes(reinterpret_cast<const CryptoPP::byte*>(key), OTP_KEY_SIZE);
+    //CryptoPP::SecByteBlock encryptedBytes(reinterpret_cast<const CryptoPP::byte*>(message.data()), message.size());
 
-    CryptoPP::SecByteBlock decryptedBytes(encryptedBytes.size());
-    CryptoPP::xorbuf(decryptedBytes, encryptedBytes, keyBytes, encryptedBytes.size());
+    //CryptoPP::SecByteBlock decryptedBytes(encryptedBytes.size());
+    //CryptoPP::xorbuf(decryptedBytes, encryptedBytes, keyBytes, encryptedBytes.size());
 
-    string decryptedText(decryptedBytes.begin(), decryptedBytes.end());
+    //string decryptedText(decryptedBytes.begin(), decryptedBytes.end());
+    string decryptedText = "";
+
+    //Doing xor between the message and the key
+    for (int i = 0; i < message.length(); i++)
+    {
+        decryptedText += (unsigned char)message[i] ^ (unsigned char)key[i];
+    }
 
     for (const auto& i : decryptedText)//delete the hex chars after the message
     {
