6db187e191f971fe0948f3d492b0886b63e920b6
Yoav Levy <yoavlevy1000@gmail.com>
2023-06-29 18:21:01 +0300

    fixed otp encryption and decryption

--- Server/OTPCryptoAlgorithm.cpp
+++ Server/OTPCryptoAlgorithm.cpp
@@ -2,7 +2,22 @@
 
 string OTPCryptoAlgorithm::encrypt(const string message, const byte* key) const
 {
-    return this->decrypt(message, key);
+    string realText;
+    //CryptoPP::SecByteBlock keyBytes(reinterpret_cast<const CryptoPP::byte*>(key), OTP_KEY_SIZE);
+    //CryptoPP::SecByteBlock encryptedBytes(reinterpret_cast<const CryptoPP::byte*>(message.data()), message.size());
+
+    //CryptoPP::SecByteBlock decryptedBytes(encryptedBytes.size());
+    //CryptoPP::xorbuf(decryptedBytes, encryptedBytes, keyBytes, encryptedBytes.size());
+
+    //string decryptedText(decryptedBytes.begin(), decryptedBytes.end());
+    string encryptedText = "";
+
+    //Doing xor between the message and the key
+    for (int i = 0; i < message.length(); i++)
+    {
+        encryptedText += (unsigned char)message[i] ^ (unsigned char)key[i];
+    }
+    return encryptedText;
 }
 
 string OTPCryptoAlgorithm::decrypt(const string message, const byte* key) const
@@ -25,12 +40,10 @@ string OTPCryptoAlgorithm::decrypt(const string message, const byte* key) const
 
     for (const auto& i : decryptedText)//delete the hex chars after the message
     {
-        realText += i;
-        if (static_cast<unsigned char>(i) > 127)
+        if (static_cast<unsigned char>(i) <= 127)
         {
-            break;
+            realText += i;
         }
-
     }
     return realText;//the text without the hex chars
 }
